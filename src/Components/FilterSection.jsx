ded0c03e6d4d4dddeeeb617aed0ff7677a03becb
Sajib <sajibdashtrg@gmail.com>
2025-11-02 01:13:03 +0600

    feat: Integrate search, category, brand, and price range filters in FilterSection component

--- src/Components/FilterSection.jsx
+++ src/Components/FilterSection.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import { getData } from "../Context/DataContext";
 import { Search } from "lucide-react";
 
-export const FilterSection = () => {
+export const FilterSection = ({
+  setSearch,
+  brand,
+  priceRange,
+  setPriceRange,
+  category,
+  handleBrandChange,
+  handleCategoryChange,
+}) => {
   const { categoryOnlyData, brandOnlyData } = getData();
 
   return (
@@ -13,6 +21,8 @@ export const FilterSection = () => {
         <input
           type="text"
           placeholder="Search products..."
+          value={Search}
+          onChange={(e) => setSearch(e.target.value)}
           className="w-full pl-10 pr-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
       </div>
@@ -28,7 +38,11 @@ export const FilterSection = () => {
             className="flex items-center gap-2 hover:bg-gray-50 p-1 rounded cursor-pointer"
           >
             <input
-              type="checkbox"
+              type="radio"
+              name={category}
+              checked={category === item}
+              value={item}
+              onChange={handleCategoryChange}
               className="accent-blue-600 w-4 h-4"
             />
             <span className="uppercase text-gray-700 text-sm">{item}</span>
@@ -41,6 +55,8 @@ export const FilterSection = () => {
         Brand
       </h2>
       <select
+        value={brand}
+        onChange={handleBrandChange}
         className="w-full p-2 border rounded-lg bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-700"
       >
         {brandOnlyData && brandOnlyData.length > 0 ? (
@@ -54,17 +70,26 @@ export const FilterSection = () => {
         )}
       </select>
       {/* price range */}
-       <h2 className="mt-6 mb-3 font-semibold text-lg text-gray-800 border-b pb-1">
+      <h2 className="mt-6 mb-3 font-semibold text-lg text-gray-800 border-b pb-1">
         Price Range
       </h2>
       <div className="flex flex-col gap-2">
         <label htmlFor="" className="text-sm">
-          Price Renge: $0 -$5000 </label>
-          <input type="range" name="" />
+          Price Renge: ${priceRange[0]} - ${priceRange[1]}{" "}
+        </label>
+        <input
+          type="range"
+          name=""
+          value={priceRange[1]}
+          onChange={(e) =>
+            setPriceRange([priceRange[0], Number(e.target.value)])
+          }
+        />
       </div>
 
-      <button className="bg-red-500 text-white rounded-md px-3 py-1 mt-5 cursor-pointer">Reset Filters</button>
-
+      <button className="bg-red-500 text-white rounded-md px-3 py-1 mt-5 cursor-pointer">
+        Reset Filters
+      </button>
     </div>
   );
 };
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getData } from "../Context/DataContext";
 import { FilterSection } from "../Components/FilterSection";
 import Loading from "../assets/Loading4.webm";
@@ -6,22 +6,53 @@ import { ProductCard } from "../Components/ProductCard";
 
 function Products() {
   const { data, fetchAllProducts } = getData();
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("All");
+  const [brand, setBrand] = useState("All");
+  const [priceRange, setPriceRange] = useState([0, 5000]);
+
   useEffect(() => {
     fetchAllProducts();
   }, []);
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+  const handleBrandChange = (e) => {
+    setBrand(e.target.value);
+  };
+
+  const filterData = data?.filter(
+    (item) =>
+      item.title.toLowerCase().includes(search.toLowerCase()) &&
+      (category === "All" || item.category === category) &&
+      (brand === "All" || item.brand === brand) &&
+      item.price >= priceRange[0] &&
+      item.price <= priceRange[1]
+  );
 
   return (
     <div>
       <div className="max-w-6xl mx-auto px-4 mb-10">
         {data?.length > 0 ? (
           <div className="flex gap-8">
-            <FilterSection></FilterSection>
+            <FilterSection
+              search={search}
+              setSearch={setSearch}
+              brand={brand}
+              setBrand={setBrand}
+              priceRange={priceRange}
+              setPriceRange={setPriceRange}
+              category={category}
+              setCategory={setCategory}
+              handleBrandChange={ handleBrandChange}
+              handleCategoryChange={ handleCategoryChange}
+            />
             <div className="grid grid-cols-1 lg:grid-cols-4 gap-7 mt-10 ">
-              {
-              data?.map ((Product,index)=>{
-                return <ProductCard key={index} Product={Product}/>
-              })
-              }</div>
+              {filterData?.map((Product, index) => {
+                return <ProductCard key={index} Product={Product} />;
+              })}
+            </div>
           </div>
         ) : (
           <div className=" flex items-center justify-center h-[400px]">
