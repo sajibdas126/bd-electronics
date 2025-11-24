006ee09a79104c79012cc83ed136227fb73ddc2c
Sajib <sajibdashtrg@gmail.com>
2025-11-24 10:00:40 +0600

    fix: Add placeholder comment in About component

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function About() {
+  // g
   return (
     <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-20">
       <div className="max-w-5xl mx-auto bg-white rounded-2xl shadow-lg p-8 space-y-8">
